9a6cca10401c8bfbce0f37346e26ba349ba103f8
Ross Brandon <rosstafarian1@gmail.com>
2021-02-03 00:03:13 -0600

    Rebuild

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Portfolio Site
 
-[![Netlify Status](https://api.netlify.com/api/v1/badges/4c4eb235-d5ec-43ae-ac5d-194cce042554/deploy-status)](https://app.netlify.com/sites/peaceful-brattain-8f67ad/deploys)
+[![Build Status](https://travis-ci.com/rosstafarian/rosstafarian.github.io.svg?branch=main)](https://travis-ci.com/rosstafarian/rosstafarian.github.io)
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,29 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const result = await graphql(`
-    query {
-      allMdx {
-        nodes {
-          frontmatter {
-            slug
-          }
+    const result = await graphql(`
+        query {
+            allMdx {
+                nodes {
+                    frontmatter {
+                        slug
+                    }
+                }
+            }
         }
-      }
+    `)
+
+    if (result.errors) {
+        reporter.panic('failed to create pages', result.errors)
     }
-  `)
 
-  if (result.errors) {
-    reporter.panic('failed to create pages', result.errors)
-  }
+    const pages = result.data.allMdx.nodes
 
-  const pages = result.data.allMdx.nodes
-
-  pages.forEach(page => {
-    actions.createPage({
-      path: page.frontmatter.slug,
-      component: require.resolve('./src/templates/page.js'),
-      context: {
-        slug: page.frontmatter.slug,
-      },
+    pages.forEach(page => {
+        actions.createPage({
+            path: page.frontmatter.slug,
+            component: require.resolve('./src/templates/page.js'),
+            context: {
+                slug: page.frontmatter.slug,
+            },
+        })
     })
-  })
 }
